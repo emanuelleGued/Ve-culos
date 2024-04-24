d4ba54dd123df2d382a2e48c5cb8c0a01f153bf4
Emanuelle Garcia Guedes <garcia.emanuelle@academico.ifpb.edu.br>
2024-04-24 18:15:35 -0300

    dicas no application

--- src/main/java/br/com/alura/tabelafipe/veiculos/VeiculosApplication.java
+++ src/main/java/br/com/alura/tabelafipe/veiculos/VeiculosApplication.java
@@ -12,6 +12,9 @@ public class VeiculosApplication implements CommandLineRunner {
 		SpringApplication.run(VeiculosApplication.class, args);
 	}
 
+	// Sobre portfólio, nós temos o 7daysofcode.io da Alura com desafios gratuitos para você desenvolver projetos em sete dias e rechear seu GitHub! Com isso você pode criar um portfólio para apresentar para empresas no futuro.
+	//
+	//Recomendamos também o techguide.sh para conferir os próximos passos de aprendizagem, para entender o que você deveria dominar após a base, no nível dois, no nível três, e assim por diante, além de assuntos paralelos que você deveria conhecer atualmente, até de outras áreas, como front-end ou dados.
 	@Override
 	public void run(String... args) throws Exception {
 		Principal principal = new Principal();
